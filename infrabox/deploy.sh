6cca0324f30f5e531b30c35b4cf1efc46929adb8
Alexander Link <33052602+alxsap@users.noreply.github.com>
2018-05-18 12:00:59 +0200

    Update deploy.sh

--- infrabox/deploy.sh
+++ infrabox/deploy.sh
@@ -16,4 +16,4 @@ neo.sh deploy-mta \
     --host "$NEO_DEPLOY_HOST" --account "$NEO_DEPLOY_ACCOUNT" \
     --user "$NEO_DEPLOY_USER" --password "$NEO_DEPLOY_PASSWORD" \
     --source "/infrabox/inputs/build/result.mtar"
-echo "Application running at https://webshopinfrabox-${NEO_DEPLOY_ACCOUNT}.dispatcher.${NEO_DEPLOY_HOST}"
+echo "Application running at https://webshopinfrabox-${NEO_DEPLOY_ACCOUNT}.dispatcher.${NEO_DEPLOY_HOST}/index.html"
